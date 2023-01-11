b3862d957f090531985ce99d6af3196f572292cc
NikolaDriver <nikolaytverdokhlebnew@gmail.com>
2023-01-11 10:35:57 +0200

    add slides

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+window.addEventListener('load', windowLoad);
+ 
+function windowLoad () {
+	if(document.querySelector('.main-slider')){
+		new Swiper('.main-slider', {
+			speed: 2000,
+			effect:"fade",
+			autoplay:{
+				delay: 3000
+			},
+			pagination: {
+				el: '.bullets__items',
+				type: 'bullets',
+				clickable: true
+			 },
+		})
+	}
+}
