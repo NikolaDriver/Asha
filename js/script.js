7101186d25af66684220e7232339147917d78348
NikolaDriver <nikolaytverdokhlebnew@gmail.com>
2023-01-14 19:40:22 +0200

    add home__related section

--- js/script.js
+++ js/script.js
@@ -41,10 +41,39 @@ function windowLoad () {
 			}
 
 		}
+		// Up
+			if (targetElement.closest('.footer__up')) {
+				window.scrollTo({
+					top: 0,
+					behavior: "smooth"
+				});
+				e.preventDefault();
+			}
 
-
-	}
+		}
+		
 	function getIndex(el) {
 		return Array.from(el.parentNode.children).indexOf(el);
 	}
+
+	//  Watcher
+
+	const items = document.querySelectorAll('[data-item]');
+	const options = {
+		threshold: 0.2
+	}
+
+	const callback = (entries) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('active');
+			}
+		});
+	}
+
+	const observer = new IntersectionObserver (callback, options);
+
+	items.forEach(item => {
+		observer.observe(item);
+	});
 }
